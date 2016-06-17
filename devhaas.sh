084ef15e4dc7e6c94f94a5a63f425d8d36439407
shwsun <shwsun@bu.edu>
2016-06-17 14:33:30 -0400

    Fixed a little bug...

---
+++ devhaas.sh
@@ -0,0 +1,11 @@
+### register node
+haas node_register sun-20 ipmi 10.10.0.20 admin Xxu46RCjtxiLdc
+
+### register switch
+haas switch_register dell-247 powerconnect55xx 172.16.10.21 admin .bluegit4
+
+### register port
+haas port_register dell-247 gi1/0/16
+
+### register nic
+haas node_register_nic sun-20 gb_0 00:09:3d:12:4b:bd
